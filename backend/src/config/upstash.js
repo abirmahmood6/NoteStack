3be9f8f3d4ee3215c5bafc86e468b4fcb8d2b610
Abir M <102193884+abirmahmood6@users.noreply.github.com>
2025-11-23 02:17:44 -0500

    rate limit updated

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -6,7 +6,7 @@ dotenv.config();  // gives access to our .env file to use environment variables.
 // create a ratelimiter that allows 10 requests per 20 seconds
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(), //Create a new Upstash Redis instance from environment variables.
-    limiter:Ratelimit.slidingWindow(20, "60 s"),
+    limiter:Ratelimit.slidingWindow(100, "60 s"), // allows 100 requests per minute
 });
 
 export default ratelimit;
